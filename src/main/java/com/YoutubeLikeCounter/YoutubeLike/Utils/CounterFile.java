3851c7efc3e98594b31aa9409a226e2a14eae3f8
BrainsOfSteel <milindsah1234@gmail.com>
2024-03-03 08:15:05 +0530

    add counter file to test the git branch rename

---
+++ src/main/java/com/YoutubeLikeCounter/YoutubeLike/Utils/CounterFile.java
@@ -0,0 +1,7 @@
+package com.YoutubeLikeCounter.YoutubeLike.Utils;
+
+public class CounterFile {
+    public static void main(String[] args) {
+        System.out.println("This is the counter file in java to test the rename feature of the branch");
+    }
+}
